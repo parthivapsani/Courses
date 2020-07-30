ea9aefb5d260c30df722ab4da826fe7d40636da1
Parthiv Chigurupati <parthivc0930@gmail.com>
2020-07-30 01:22:54 -0400

    Finished gsm and cleaning

--- Google Algorithms/Assignments/Week 3/gsm_network.cpp
+++ Google Algorithms/Assignments/Week 3/gsm_network.cpp
@@ -10,6 +10,7 @@ struct Edge {
 };
 
 struct ConvertGSMNetworkProblemToSat {
+    const int frequencyCount = 3;
     int numVertices;
     vector<Edge> edges;
 
@@ -18,28 +19,27 @@ struct ConvertGSMNetworkProblemToSat {
         edges(m)
     {  }
 
-    void printEquisatisfiableSatFormula() {
-        // This solution prints a simple satisfiable formula
-        // and passes about half of the tests.
-        // Change this function to solve the problem.
-        cout << "3 2" << endl;
-        cout << "1 2 0" << endl;
-        cout << "-1 -2 0" << endl;
-        cout << "1 -2 0" << endl;
+    void printEquisatisfiableSatFormula() const {
+        printf("%lu %d\n", frequencyCount * edges.size() + numVertices, frequencyCount * numVertices);  // Classes and variables
+        for (int x = 0, bandCount = 1; x < numVertices; ++x, bandCount += frequencyCount) {
+            printf("%d %d %d 0\n", bandCount, bandCount + 1, bandCount + 2);
+        }
+        for (const Edge edge : edges) {
+            for (int x = 1; x <= frequencyCount; ++x) {
+                printf("%d %d 0\n", -((edge.from - 1) * frequencyCount + x), -((edge.to - 1) * frequencyCount + x));
+            }
+        }
     }
 };
 
 int main() {
     ios::sync_with_stdio(false);
-
     int n, m;
     cin >> n >> m;
     ConvertGSMNetworkProblemToSat converter(n, m);
     for (int i = 0; i < m; ++i) {
         cin >> converter.edges[i].from >> converter.edges[i].to;
     }
-
     converter.printEquisatisfiableSatFormula();
-
     return 0;
 }
