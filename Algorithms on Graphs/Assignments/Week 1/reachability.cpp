0c070a9f49062e8e176f13444eba06e4fd326450
Parthiv Chigurupati <parthivc0930@gmail.com>
2020-08-09 12:51:46 -0400

    Finished week 1

--- Algorithms on Graphs/Assignments/Week 1/reachability.cpp
+++ Algorithms on Graphs/Assignments/Week 1/reachability.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-using std::vector;
-using std::pair;
+using namespace std;
 
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+int reach(vector<vector<int>> &adj, int x, int y) {
+    vector<bool> visited(adj.size(), false);
+    visited[x] = true;
+    queue<int> bfs;
+    bfs.push(x);
+    while (!bfs.empty()) {
+        int current = bfs.front();
+        bfs.pop();
+        if (current == y) {
+            break;
+        }
+        for (int node : adj[current]) {
+            if (!visited[node]) {
+                visited[node] = true;
+                bfs.push(node);
+            }
+        }
+    }
+    return visited[y] ? 1 : 0;
 }
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+    size_t n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n, vector<int>());
+    for (size_t i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
     int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int x, y;
-  std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+    cin >> x >> y;
+    cout << reach(adj, x - 1, y - 1);
 }
---
+++ Algorithms on Graphs/Assignments/cmake-build-debug/CMakeFiles/Assignments.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignments"
+  "Assignments.pdb"
+  "CMakeFiles/Assignments.dir/Week_1/connected_components.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
